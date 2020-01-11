253bbc0e3eb3678a516ba6f4dc306faf9f981283
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-11 20:49:59 +1100

    Update properties

--- FunctionalVarietyTracker/FunctionalVarietyTracker/Properties/AssemblyInfo.cs
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/Properties/AssemblyInfo.cs
@@ -32,12 +32,12 @@ using System.Windows;
 
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
+  ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+  //(used if a resource is not found in the page,
+  // or application resource dictionaries)
+  ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+  //(used if a resource is not found in the page,
+  // app, or any theme specific resource dictionaries)
 )]
 
 
@@ -53,3 +53,6 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("FunctionalVarietyTracker.Tests")]
+[assembly: NeutralResourcesLanguage("en")]
