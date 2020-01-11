ae3a7c47ef2e50ab1137a3539ff5627961696937
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-11 20:48:14 +1100

    Created DependencyShell

---
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/DependencyShell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FunctionalVarietyTracker.AutoDependencies;
+
+namespace FunctionalVarietyTracker
+{
+  internal static class DependencyShell
+  {
+    public static IAutoDependency BuildDependencies()
+    {
+      var autoDependency = AutoDependency.Instance;
+
+      var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+      autoDependency.RegisterAutoDependencies(assemblies);
+
+      autoDependency.BuildContainer();
+      return autoDependency;
+    }
+  }
+}
