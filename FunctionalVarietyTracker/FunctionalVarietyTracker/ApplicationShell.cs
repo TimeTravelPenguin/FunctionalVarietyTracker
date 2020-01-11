e06406764662c77b25bfd1d8189c004d1d4b1e4a
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-11 20:51:07 +1100

    Created ApplicationShell

---
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/ApplicationShell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FunctionalVarietyTracker.AutoDependencies;
+using FunctionalVarietyTracker.Properties;
+using FunctionalVarietyTracker.ViewModels;
+using FunctionalVarietyTracker.Views;
+
+namespace FunctionalVarietyTracker
+{
+  internal class ApplicationShell
+  {
+    private static IAutoDependency _autoDependency;
+
+    private ApplicationShell()
+    {
+      _autoDependency = DependencyShell.BuildDependencies();
+    }
+
+    public static ApplicationShell NewShell()
+    {
+      return new ApplicationShell();
+    }
+
+    public void Startup()
+    {
+      if (_autoDependency is null)
+      {
+        throw new InvalidOperationException(ExceptionResources.AutoDependency_NotInitialised);
+      }
+
+      var setup = new SetupWindow
+      {
+        DataContext = new SetupViewModel(_autoDependency)
+      };
+
+      setup.Show();
+    }
+  }
+}
