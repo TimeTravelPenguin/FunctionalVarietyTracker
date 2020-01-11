f188b6b5f516d9e626bd1e2c712fb9801d390573
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-11 20:50:16 +1100

    Created SetupViewModel

---
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/ViewModels/SetupViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using FunctionalVarietyTracker.AutoDependencies;
+using FunctionalVarietyTracker.Models.GameData;
+using FunctionalVarietyTracker.Types;
+
+namespace FunctionalVarietyTracker.ViewModels
+{
+  internal class SetupViewModel : PropertyChangedBase
+  {
+    private static IAutoDependency _autoDependency;
+    private ObservableCollection<IGameDataModel> _gameDataModels;
+
+    public ObservableCollection<IGameDataModel> GameDataModels
+    {
+      get => _gameDataModels;
+      set => SetValue(ref _gameDataModels, value);
+    }
+    
+    public SetupViewModel(IAutoDependency autoDependency) : this()
+    {
+      _autoDependency = autoDependency;
+    }
+
+    public SetupViewModel()
+    {
+      // TODO : Find json files / game data for randomizer amd bingo
+    }
+  }
+}
