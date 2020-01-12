1f23b4d0d02bf5482156dd3c75e52380602dde9c
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-12 23:01:18 +1100

    Added viewmodels

---
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/ViewModels/EditGameViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FunctionalVarietyTracker.Models.GameData;
+using FunctionalVarietyTracker.Types;
+
+namespace FunctionalVarietyTracker.ViewModels
+{
+  internal class EditGameViewModel : PropertyChangedBase
+  {
+    private IGameDataModel _gameData = new GameDataModel();
+
+    public IGameDataModel GameData
+    {
+      get => _gameData;
+      set => SetValue(ref _gameData, value);
+    }
+
+    public EditGameViewModel()
+    {
+      GameData.Categories.AddItem(new CategoryModel("My category 01"));
+      GameData.Categories.AddItem(new CategoryModel("My category 02"));
+      GameData.Categories.AddItem(new CategoryModel("My category 03"));
+    }
+  }
+}
--- FunctionalVarietyTracker/FunctionalVarietyTracker/ViewModels/SetupViewModel.cs
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/ViewModels/SetupViewModel.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections.ObjectModel;
 using FunctionalVarietyTracker.AutoDependencies;
+using FunctionalVarietyTracker.Models;
 using FunctionalVarietyTracker.Models.GameData;
 using FunctionalVarietyTracker.Types;
+using FunctionalVarietyTracker.Views;
+using Microsoft.Expression.Interactivity.Core;
 
 namespace FunctionalVarietyTracker.ViewModels
 {
   internal class SetupViewModel : PropertyChangedBase
   {
     private static IAutoDependency _autoDependency;
+    private SelectableCollectionModel<IGameDataModel> _gameCollectionFromFiles;
     private ObservableCollection<IGameDataModel> _gameDataModels;
+
+    public ActionCommand CommandCreateNew { get; }
+
+    public SelectableCollectionModel<IGameDataModel> GameCollectionFromFiles
+    {
+      get => _gameCollectionFromFiles;
+      set => SetValue(ref _gameCollectionFromFiles, value);
+    }
 
     public ObservableCollection<IGameDataModel> GameDataModels
     {
       get => _gameDataModels;
       set => SetValue(ref _gameDataModels, value);
     }
-    
+
     public SetupViewModel(IAutoDependency autoDependency) : this()
     {
       _autoDependency = autoDependency;
@@ -23,7 +35,17 @@ namespace FunctionalVarietyTracker.ViewModels
 
     public SetupViewModel()
     {
-      // TODO : Find json files / game data for randomizer amd bingo
+      CommandCreateNew = new ActionCommand(CreateNewGameData);
+    }
+
+    private static void CreateNewGameData()
+    {
+      var view = new EditGameView
+      {
+        DataContext = new EditGameViewModel()
+      };
+
+      view.ShowDialog();
     }
   }
 }
