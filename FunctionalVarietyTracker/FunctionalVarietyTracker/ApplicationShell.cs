34126ee882aa84038e667bc989fa9dfaee2e4f53
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-12 23:01:51 +1100

    Added code to check for saves directory in exe root

--- FunctionalVarietyTracker/FunctionalVarietyTracker/ApplicationShell.cs
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/ApplicationShell.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
 using FunctionalVarietyTracker.AutoDependencies;
 using FunctionalVarietyTracker.Properties;
 using FunctionalVarietyTracker.ViewModels;
@@ -27,12 +32,42 @@ namespace FunctionalVarietyTracker
         throw new InvalidOperationException(ExceptionResources.AutoDependency_NotInitialised);
       }
 
+      InitialiseWorkingDirectory();
+
       var setup = new SetupWindow
       {
         DataContext = new SetupViewModel(_autoDependency)
       };
 
       setup.Show();
+    }
+
+    private void InitialiseWorkingDirectory()
+    {
+      var path = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+      var subDir = Path.Combine(path ?? throw new NullReferenceException(ExceptionResources.FilePathNull), "GameData");
+
+      // Check folder for loading data exists
+      if (!Directory.Exists(subDir))
+      {
+        Directory.CreateDirectory(subDir);
+      }
+
+      // Check for files
+      var files = Directory.GetFiles(subDir);
+
+      // TODO: Process files and report non-working files
+      if (files.Any(f => f.EndsWith(Resources.FileTypeExtension)))
+      {
+        return;
+      }
+
+      if (MessageBox.Show(
+            Resources.NoFilesFound_Dialogue,
+            Resources.NoFilesFound, MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+      {
+        Process.Start(Resources.GitHubRepo);
+      }
     }
   }
 }
