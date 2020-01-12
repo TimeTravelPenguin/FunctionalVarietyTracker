46b534b8010c8503bfda8ad86744807f6282d76c
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-12 23:01:28 +1100

    Added views

---
+++ FunctionalVarietyTracker/FunctionalVarietyTracker/Views/EditGameView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FunctionalVarietyTracker.Views
+{
+  /// <summary>
+  ///   Interaction logic for EditGameView.xaml
+  /// </summary>
+  public partial class EditGameView : Window
+  {
+    public EditGameView()
+    {
+      InitializeComponent();
+    }
+  }
+}
